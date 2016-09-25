bbdf6eba9d810291d4141a53033e624f18a018fc
Oleg Blokhin <blohin.o.d@gmail.com>
2016-09-25 22:25:58 +0300

    Implementation simple fibonaccy numbers calculation strategy

---
+++ PT_Interview/Domain/FibonacciCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class FibonacciCalculator
+    {
+        public int GetNumberBySerialNumber(int serialNumber)
+        {
+            if (serialNumber == 0) return 0;
+            if (serialNumber == 1) return 1;
+            if (serialNumber == 2) return 1;
+            return GetNumberBySerialNumber(serialNumber - 1) + GetNumberBySerialNumber(serialNumber - 2);
+        }
+    }
+}
---
+++ PT_Interview/Tests/WhenCalculateFibonacci.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class WhenCalculateFibonacci
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _fibonacciCalculator = new FibonacciCalculator();
+        }
+
+        private FibonacciCalculator _fibonacciCalculator;
+
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(15, ExpectedResult = 610)]
+        public int ReturnFibonacciNumberBySerialNumber(int serialNumber)
+        {
+            return _fibonacciCalculator.GetNumberBySerialNumber(serialNumber);
+        }
+    }
+}
