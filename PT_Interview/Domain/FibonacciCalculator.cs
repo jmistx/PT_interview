381cbb47d37aae54e43be1bb8f70e48bd0f6a581
Oleg Blokhin <blohin.o.d@gmail.com>
2016-09-25 22:47:47 +0300

    ImplementingFibonacciCalculationLogic

--- PT_Interview/Domain/FibonacciCalculator.cs
+++ PT_Interview/Domain/FibonacciCalculator.cs
@@ -1,13 +1,32 @@
-﻿namespace Domain
+﻿using System;
+
+namespace Domain
 {
     public class FibonacciCalculator
     {
-        public int GetNumberBySerialNumber(int serialNumber)
+        public int GetNumberByPreviousNumber(int previousNumber)
         {
-            if (serialNumber == 0) return 0;
-            if (serialNumber == 1) return 1;
-            if (serialNumber == 2) return 1;
-            return GetNumberBySerialNumber(serialNumber - 1) + GetNumberBySerialNumber(serialNumber - 2);
+            if (previousNumber <= 0)
+            {
+                return 0;
+            }
+
+            int prev = 1;
+            int current = 1;
+            int next = 2;
+            while (current < previousNumber)
+            {
+                prev = current;
+                current = next;
+                next = prev + current;
+            }
+
+            if (current == previousNumber)
+            {
+                return next;
+            }
+
+            throw new ArgumentException();
         }
     }
 }
--- PT_Interview/Tests/WhenCalculateFibonacci.cs
+++ PT_Interview/Tests/WhenCalculateFibonacci.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using System;
+using Domain;
 using NUnit.Framework;
 
 namespace Tests
@@ -14,13 +15,23 @@ namespace Tests
 
         private FibonacciCalculator _fibonacciCalculator;
 
-        [TestCase(1, ExpectedResult = 1)]
-        [TestCase(2, ExpectedResult = 1)]
-        [TestCase(3, ExpectedResult = 2)]
-        [TestCase(15, ExpectedResult = 610)]
-        public int ReturnFibonacciNumberBySerialNumber(int serialNumber)
+        [TestCase(-1, ExpectedResult = 0)]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 2)]
+        [TestCase(2, ExpectedResult = 3)]
+        [TestCase(3, ExpectedResult = 5)]
+        [TestCase(4181, ExpectedResult = 6765)]
+        [Test]
+        public int ReturnFibonacciNumberByPreviousFibonacciNumber(int previousNumber)
         {
-            return _fibonacciCalculator.GetNumberBySerialNumber(serialNumber);
+            return _fibonacciCalculator.GetNumberByPreviousNumber(previousNumber);
+        }
+
+        [TestCase(6)]
+        [TestCase(54)]
+        public void ThrowsForNonFibonaccyNumbers(int previousNumber)
+        {
+            Assert.Throws<ArgumentException>(() => _fibonacciCalculator.GetNumberByPreviousNumber(previousNumber));
         }
     }
 }
